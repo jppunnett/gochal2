396c1316eb992d76620b03718c83732924de98b8
James Punnett <jppunnett@yahoo.com>
2015-05-08 05:30:38 -0400

    Initial commit

--- main.go
+++ main.go
@@ -1,65 +1,277 @@
 package main
 
 import (
-        "flag"
-        "fmt"
-        "io"
-        "log"
-        "net"
-        "os"
+	"crypto/rand"
+	"flag"
+	"fmt"
+	"golang.org/x/crypto/nacl/box"
+	"io"
+	"log"
+	"net"
+	"os"
 )
 
+const (
+	noncesz = 24
+	keysz   = 32
+)
+
+// secureReader implements the io.Reader interface to read and decrypt messages.
+type secureReader struct {
+	r         io.Reader
+	priv, pub *[keysz]byte
+}
+
+// Read reads encrypted bytes from the Reader, decrypts the bytes and copies
+// decrypted bytes to p.
+func (sr *secureReader) Read(p []byte) (int, error) {
+	if len(p) == 0 {
+		return 0, nil
+	}
+
+	//	The first noncesz bytes should be the nonce
+	var nonce [noncesz]byte
+	n, err := io.ReadFull(sr.r, nonce[:])
+	if err != nil {
+		return n, err
+	}
+	if n != noncesz {
+		return n, fmt.Errorf("secureReader.Read: Unexpected nonce length: %d", n)
+	}
+
+	// Buffer has to be at least len(p) + encryption overhead.
+	encrptd := make([]byte, len(p)+box.Overhead)
+	n, err = sr.r.Read(encrptd)
+	if err != nil {
+		return n, err
+	}
+	// TODO: Must handle scenario where n < len(encrptd)
+
+	decrypted, ok := box.Open(nil, encrptd[:n], &nonce, sr.pub, sr.priv)
+	if !ok {
+		return n, fmt.Errorf("secureReader.Read: Error decrypting data")
+	}
+
+	return copy(p, decrypted), nil
+}
+
 // NewSecureReader instantiates a new SecureReader
-func NewSecureReader(r io.Reader, priv, pub *[32]byte) io.Reader {
-        return nil
+func NewSecureReader(r io.Reader, priv, pub *[keysz]byte) io.Reader {
+	return &secureReader{r, priv, pub}
+}
+
+// secureWriter implements the io.Writer interface to write encrypted messages.
+type secureWriter struct {
+	w         io.Writer
+	priv, pub *[keysz]byte
+}
+
+// Write encrypts the bytes in p then copies the encrytped bytes to the Writer.
+func (sw *secureWriter) Write(p []byte) (int, error) {
+	if len(p) == 0 {
+		return 0, nil
+	}
+
+	var nonce [noncesz]byte
+	n, err := rand.Read(nonce[:])
+	if err != nil {
+		return 0, fmt.Errorf("secureWriter.Write: %v", err)
+	}
+	if n != noncesz {
+		return 0, fmt.Errorf("secureWriter.Write: only generated %d bytes for nouce", n)
+	}
+
+	//	Write the nonce. This is in the clear.
+	n, err = sw.w.Write(nonce[:])
+	if err != nil {
+		return n, fmt.Errorf("secureWriter.Write: %v", err)
+	}
+	if n != noncesz {
+		return 0, fmt.Errorf("secureWriter.Write: only wrote %d bytes for nouce", n)
+	}
+
+	return sw.w.Write(box.Seal(nil, p, &nonce, sw.pub, sw.priv))
 }
 
 // NewSecureWriter instantiates a new SecureWriter
-func NewSecureWriter(w io.Writer, priv, pub *[32]byte) io.Writer {
-        return nil
+func NewSecureWriter(w io.Writer, priv, pub *[keysz]byte) io.Writer {
+	return &secureWriter{w, priv, pub}
 }
 
-// Dial generates a private/public key pair,
-// connects to the server, perform the handshake
-// and return a reader/writer.
+// secureReadWriter implements the io.ReadWriteCloser interface to read and
+// write secure messages.
+type secureReadWriter struct {
+	rwc io.ReadWriteCloser
+	sw  io.Writer
+	sr  io.Reader
+}
+
+// NewSecureReadWriter instantiates a new secureReadWriter
+func NewSecureReadWriter(rwc io.ReadWriteCloser, priv, pub *[keysz]byte) io.ReadWriteCloser {
+	return &secureReadWriter{
+		rwc,
+		NewSecureWriter(rwc, priv, pub),
+		NewSecureReader(rwc, priv, pub),
+	}
+}
+
+func (srw *secureReadWriter) Read(p []byte) (int, error) {
+	return srw.sr.Read(p)
+}
+
+func (srw *secureReadWriter) Write(p []byte) (int, error) {
+	return srw.sw.Write(p)
+}
+
+func (srw *secureReadWriter) Close() error {
+	return srw.rwc.Close()
+}
+
+// Dial generates a private/public key pair, connects to the server, perform
+// the handshake and return a reader/writer.
 func Dial(addr string) (io.ReadWriteCloser, error) {
-        return nil, nil
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		return nil, err
+	}
+	defer func(c net.Conn, e error) {
+		if e != nil {
+			fmt.Printf("Dial: Closing connection because: %v", err)
+			c.Close()
+		}
+	}(conn, err)
+
+	// Receive public key from server. The client uses the server's public key
+	//	and its private key to encrypt/decrypt messages.
+	var srvpub [keysz]byte
+	n, err := conn.Read(srvpub[:])
+	if err != nil {
+		return nil, err
+	}
+	if n != keysz {
+		return nil, fmt.Errorf("Dial: could only read <%d> bytes of server's public key.", n)
+	}
+
+	// Generate key-pair for public key exchange (handshake)
+	pub, priv, err := box.GenerateKey(rand.Reader)
+	if err != nil {
+		return nil, err
+	}
+
+	// Send public key to server. The server uses the client's public key, along
+	//	with the server's private key to encrypt/decrypt messages.
+	n, err = conn.Write(pub[:])
+	if err != nil {
+		return nil, err
+	}
+	if n != keysz {
+		return nil, fmt.Errorf("Dial: could only write <%d> bytes of client's public key.", n)
+	}
+
+	return NewSecureReadWriter(conn, priv, &srvpub), nil
 }
 
 // Serve starts a secure echo server on the given listener.
 func Serve(l net.Listener) error {
-        return nil
+	// Generate key-pair for public key exchange (handshake)
+	pub, priv, err := box.GenerateKey(rand.Reader)
+	if err != nil {
+		return err
+	}
+
+	// Wait for and handle incoming connections.
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			return err
+		}
+		go handleConnection(conn, priv, pub)
+	}
+}
+
+func handleConnection(conn net.Conn, pri, pub *[keysz]byte) {
+	//	Send public key to client. The client will use the server's public key
+	//	along with its own private key to encrypt/decrypt messages.
+
+	// TODO Clean up. Don't like all the repetative error handling code for key
+	// exchange.
+	n, err := conn.Write(pub[:])
+	if err != nil {
+		conn.Close()
+		fmt.Printf("handleConnection: %v\n", err)
+		return
+	}
+	if n != keysz {
+		conn.Close()
+		fmt.Printf("handleConnection: could only write <%d> bytes of server's public key.\n", n)
+		return
+	}
+
+	// First keysz bytes read should be the public key of the connecting client
+	var clipub [keysz]byte
+	n, err = conn.Read(clipub[:])
+	if err != nil {
+		conn.Close()
+		fmt.Printf("handleConnection.io.conn.Read: %v\n", err)
+		return
+	}
+	if n != keysz {
+		conn.Close()
+		fmt.Printf("handleConnection: could only read <%d> bytes of client's public key.\n", n)
+		return
+	}
+
+	// Key exchange complete
+	swr := NewSecureReadWriter(conn, pri, &clipub)
+	defer swr.Close()
+
+	//	Read message from client, echo it back to them, and exit.
+	buf := make([]byte, 2048)
+	n, err = swr.Read(buf)
+	if err != nil && err != io.EOF {
+		fmt.Printf("handleConnection.swr.Read: %v\n", err)
+		return
+	}
+
+	// Echo
+	n, err = swr.Write(buf[:n])
+	if err != nil {
+		fmt.Printf("handleConnection.swr.Write: %v\n", err)
+		return
+	}
+
+	// TODO Extend to echo until client wants to stop or connection times out.
 }
 
 func main() {
-        port := flag.Int("l", 0, "Listen mode. Specify port")
-        flag.Parse()
+	port := flag.Int("l", 0, "Listen mode. Specify port")
+	flag.Parse()
 
-        // Server mode
-        if *port != 0 {
-                l, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-                if err != nil {
-                        log.Fatal(err)
-                }
-                defer l.Close()
-                log.Fatal(Serve(l))
-        }
+	// Server mode
+	if *port != 0 {
+		l, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer l.Close()
+		log.Fatal(Serve(l))
+	}
 
-        // Client mode
-        if len(os.Args) != 3 {
-                log.Fatalf("Usage: %s <port> <message>", os.Args[0])
-        }
-        conn, err := Dial("localhost:" + os.Args[1])
-        if err != nil {
-                log.Fatal(err)
-        }
-        if _, err := conn.Write([]byte(os.Args[2])); err != nil {
-                log.Fatal(err)
-        }
-        buf := make([]byte, len(os.Args[2]))
-        n, err := conn.Read(buf)
-        if err != nil && err != io.EOF {
-                log.Fatal(err)
-        }
-        fmt.Printf("%s\n", buf[:n])
+	// Client mode
+	if len(os.Args) != 3 {
+		log.Fatalf("Usage: %s <port> <message>", os.Args[0])
+	}
+	conn, err := Dial("localhost:" + os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := conn.Write([]byte(os.Args[2])); err != nil {
+		log.Fatal(err)
+	}
+	buf := make([]byte, len(os.Args[2]))
+	n, err := conn.Read(buf)
+	if err != nil && err != io.EOF {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", buf[:n])
 }
--- main_test.go
+++ main_test.go
@@ -1,186 +1,272 @@
 package main
 
 import (
-        "fmt"
-        "io"
-        "io/ioutil"
-        "net"
-        "testing"
+	"crypto/rand"
+	"fmt"
+	"golang.org/x/crypto/nacl/box"
+	"io"
+	"io/ioutil"
+	"net"
+	"testing"
 )
 
+func TestBasicConcepts(t *testing.T) {
+	// Client key pair
+	cpub, cpri, err := box.GenerateKey(rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Server key pair
+	spub, spri, err := box.GenerateKey(rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cmsg := "hello server"
+	smsg := "hello client"
+
+	// buf := make([]byte, 1024)
+	nonce := [24]byte{1}
+
+	// Encrypt client message using server public key and client private key
+	encrypted := box.Seal(nil, []byte(cmsg), &nonce, spub, cpri)
+
+	// decrypt client message using client public key and server private key
+	decrypted, _ := box.Open(nil, encrypted, &nonce, cpub, spri)
+
+	if string(decrypted) != cmsg {
+		t.Fatalf("Unexpected result: %s != %s", decrypted, cmsg)
+	}
+
+	// Encrypt the server msg using client public key and server private key
+	encrypted = box.Seal(nil, []byte(smsg), &nonce, cpub, spri)
+
+	// decrypt the server msg server public key and client private key
+	decrypted, _ = box.Open(nil, encrypted, &nonce, spub, cpri)
+	if string(decrypted) != smsg {
+		t.Fatalf("Unexpected result: %s != %s", decrypted, smsg)
+	}
+}
+
 func TestReadWriterPing(t *testing.T) {
-        priv, pub := &[32]byte{'p', 'r', 'i', 'v'}, &[32]byte{'p', 'u', 'b'}
+	priv, pub := &[32]byte{'p', 'r', 'i', 'v'}, &[32]byte{'p', 'u', 'b'}
 
-        r, w := io.Pipe()
-        secureR := NewSecureReader(r, priv, pub)
-        secureW := NewSecureWriter(w, priv, pub)
+	r, w := io.Pipe()
+	secureR := NewSecureReader(r, priv, pub)
+	secureW := NewSecureWriter(w, priv, pub)
 
-        // Encrypt hello world
-        go func() {
-                fmt.Fprintf(secureW, "hello world\n")
-                w.Close()
-        }()
+	// Encrypt hello world
+	go func() {
+		fmt.Fprintf(secureW, "hello world\n")
+		w.Close()
+	}()
 
-        // Decrypt message
-        buf := make([]byte, 1024)
-        n, err := secureR.Read(buf)
-        if err != nil && err != io.EOF {
-                t.Fatal(err)
-        }
-        buf = buf[:n]
+	// Decrypt message
+	buf := make([]byte, 1024)
+	n, err := secureR.Read(buf)
+	if err != nil && err != io.EOF {
+		t.Fatal(err)
+	}
+	buf = buf[:n]
 
-        // Make sure we have hello world back
-        if res := string(buf); res != "hello world\n" {
-                t.Fatalf("Unexpected result: %s != %s", res, "hello world")
-        }
+	// Make sure we have hello world back
+	if res := string(buf); res != "hello world\n" {
+		t.Fatalf("Unexpected result: %s != %s", res, "hello world")
+	}
 }
 
 func TestSecureWriter(t *testing.T) {
-        priv, pub := &[32]byte{'p', 'r', 'i', 'v'}, &[32]byte{'p', 'u', 'b'}
+	priv, pub := &[32]byte{'p', 'r', 'i', 'v'}, &[32]byte{'p', 'u', 'b'}
 
-        r, w := io.Pipe()
-        secureW := NewSecureWriter(w, priv, pub)
+	r, w := io.Pipe()
+	secureW := NewSecureWriter(w, priv, pub)
 
-        // Make sure we are secure
-        // Encrypt hello world
-        go func() {
-                fmt.Fprintf(secureW, "hello world\n")
-                w.Close()
-        }()
+	// Make sure we are secure
+	// Encrypt hello world
+	go func() {
+		fmt.Fprintf(secureW, "hello world\n")
+		w.Close()
+	}()
 
-        // Read from the underlying transport instead of the decoder
-        buf, err := ioutil.ReadAll(r)
-        if err != nil {
-                t.Fatal(err)
-        }
-        // Make sure we dont' read the plain text message.
-        if res := string(buf); res == "hello world\n" {
-                t.Fatal("Unexpected result. The message is not encrypted.")
-        }
+	// Read from the underlying transport instead of the decoder
+	buf, err := ioutil.ReadAll(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Make sure we dont' read the plain text message.
+	if res := string(buf); res == "hello world\n" {
+		t.Fatal("Unexpected result. The message is not encrypted.")
+	}
 
-        r, w = io.Pipe()
-        secureW = NewSecureWriter(w, priv, pub)
+	r, w = io.Pipe()
+	secureW = NewSecureWriter(w, priv, pub)
 
-        // Make sure we are unique
-        // Encrypt hello world
-        go func() {
-                fmt.Fprintf(secureW, "hello world\n")
-                w.Close()
-        }()
+	// Make sure we are unique
+	// Encrypt hello world
+	go func() {
+		fmt.Fprintf(secureW, "hello world\n")
+		w.Close()
+	}()
 
-        // Read from the underlying transport instead of the decoder
-        buf2, err := ioutil.ReadAll(r)
-        if err != nil {
-                t.Fatal(err)
-        }
-        // Make sure we dont' read the plain text message.
-        if string(buf) == string(buf2) {
-                t.Fatal("Unexpected result. The encrypted message is not unique.")
-        }
+	// Read from the underlying transport instead of the decoder
+	buf2, err := ioutil.ReadAll(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Make sure we dont' read the plain text message.
+	if string(buf) == string(buf2) {
+		t.Fatal("Unexpected result. The encrypted message is not unique.")
+	}
 
 }
 
 func TestSecureEchoServer(t *testing.T) {
-        // Create a random listener
-        l, err := net.Listen("tcp", "127.0.0.1:0")
-        if err != nil {
-                t.Fatal(err)
-        }
-        defer l.Close()
+	// Create a random listener
+	l, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer l.Close()
 
-        // Start the server
-        go Serve(l)
+	// Start the server
+	go Serve(l)
 
-        conn, err := Dial(l.Addr().String())
-        if err != nil {
-                t.Fatal(err)
-        }
-        defer conn.Close()
+	conn, err := Dial(l.Addr().String())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
 
-        expected := "hello world\n"
-        if _, err := fmt.Fprintf(conn, expected); err != nil {
-                t.Fatal(err)
-        }
+	expected := "hello world\n"
+	if _, err := fmt.Fprintf(conn, expected); err != nil {
+		t.Fatal(err)
+	}
 
-        buf := make([]byte, 2048)
-        n, err := conn.Read(buf)
-        if err != nil && err != io.EOF {
-                t.Fatal(err)
-        }
+	buf := make([]byte, 2048)
+	n, err := conn.Read(buf)
+	if err != nil && err != io.EOF {
+		t.Fatal(err)
+	}
 
-        if got := string(buf[:n]); got != expected {
-                t.Fatalf("Unexpected result:\nGot:\t\t%s\nExpected:\t%s\n", got, expected)
-        }
+	if got := string(buf[:n]); got != expected {
+		t.Fatalf("Unexpected result:\nGot:\t\t%s\nExpected:\t%s\n", got, expected)
+	}
 }
 
 func TestSecureServe(t *testing.T) {
-        // Create a random listener
-        l, err := net.Listen("tcp", "127.0.0.1:0")
-        if err != nil {
-                t.Fatal(err)
-        }
-        defer l.Close()
+	// Create a random listener
+	l, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer l.Close()
 
-        // Start the server
-        go Serve(l)
+	// Start the server
+	go Serve(l)
 
-        conn, err := net.Dial("tcp", l.Addr().String())
-        if err != nil {
-                t.Fatal(err)
-        }
-        unexpected := "hello world\n"
-        if _, err := fmt.Fprintf(conn, unexpected); err != nil {
-                t.Fatal(err)
-        }
-        buf := make([]byte, 2048)
-        n, err := conn.Read(buf)
-        if err != nil && err != io.EOF {
-                t.Fatal(err)
-        }
-        if got := string(buf[:n]); got == unexpected {
-                t.Fatalf("Unexpected result:\nGot raw data instead of serialized key")
-        }
+	conn, err := net.Dial("tcp", l.Addr().String())
+	if err != nil {
+		t.Fatal(err)
+	}
+	unexpected := "hello world\n"
+	if _, err := fmt.Fprintf(conn, unexpected); err != nil {
+		t.Fatal(err)
+	}
+	buf := make([]byte, 2048)
+	n, err := conn.Read(buf)
+	if err != nil && err != io.EOF {
+		t.Fatal(err)
+	}
+	if got := string(buf[:n]); got == unexpected {
+		t.Fatalf("Unexpected result:\nGot raw data instead of serialized key")
+	}
 }
 
 func TestSecureDial(t *testing.T) {
-        // Create a random listener
-        l, err := net.Listen("tcp", "127.0.0.1:0")
-        if err != nil {
-                t.Fatal(err)
-        }
-        defer l.Close()
+	// Create a random listener
+	l, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer l.Close()
 
-        // Start the server
-        go func(l net.Listener) {
-                for {
-                        conn, err := l.Accept()
-                        if err != nil {
-                                return
-                        }
-                        go func(c net.Conn) {
-                                defer c.Close()
-                                key := [32]byte{}
-                                c.Write(key[:])
-                                buf := make([]byte, 2048)
-                                n, err := c.Read(buf)
-                                if err != nil && err != io.EOF {
-                                        t.Fatal(err)
-                                }
-                                if got := string(buf[:n]); got == "hello world\n" {
-                                        t.Fatal("Unexpected result. Got raw data instead of encrypted")
-                                }
-                        }(conn)
-                }
-        }(l)
+	// Start the server
+	go func(l net.Listener) {
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				return
+			}
+			go func(c net.Conn) {
+				defer c.Close()
+				key := [32]byte{}
+				c.Write(key[:])
+				buf := make([]byte, 2048)
+				n, err := c.Read(buf)
+				if err != nil && err != io.EOF {
+					t.Fatal(err)
+				}
+				if got := string(buf[:n]); got == "hello world\n" {
+					t.Fatal("Unexpected result. Got raw data instead of encrypted")
+				}
+			}(conn)
+		}
+	}(l)
 
-        conn, err := Dial(l.Addr().String())
-        if err != nil {
-                t.Fatal(err)
-        }
-        defer conn.Close()
+	conn, err := Dial(l.Addr().String())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
 
-        expected := "hello world\n"
-        if _, err := fmt.Fprintf(conn, expected); err != nil {
-                t.Fatal(err)
-        }
+	expected := "hello world\n"
+	if _, err := fmt.Fprintf(conn, expected); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestMoreThanOneExchange(t *testing.T) {
+	// Create a random listener
+	l, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer l.Close()
+
+	// Start the server
+	go Serve(l)
+
+	conn, err := Dial(l.Addr().String())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
+
+	expected := "hello world\n"
+	if _, err := fmt.Fprintf(conn, expected); err != nil {
+		t.Fatal(err)
+	}
+
+	buf := make([]byte, 2048)
+	n, err := conn.Read(buf)
+	if err != nil && err != io.EOF {
+		t.Fatal(err)
+	}
+
+	if got := string(buf[:n]); got != expected {
+		t.Fatalf("Unexpected result:\nGot:\t\t%s\nExpected:\t%s\n", got, expected)
+	}
+
+	expected = "hello world again!\n"
+	if _, err := fmt.Fprintf(conn, expected); err != nil {
+		t.Fatal(err)
+	}
+	n, err = conn.Read(buf)
+	if err != nil && err != io.EOF {
+		t.Fatal(err)
+	}
+
+	if got := string(buf[:n]); got != expected {
+		t.Fatalf("Unexpected result:\nGot:\t\t%s\nExpected:\t%s\n", got, expected)
+	}
 }
